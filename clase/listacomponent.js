b02861cf3249ff952e0a44f5ea3fc04dbab73e5f
Alberto Basalo <albertobasalo@hotmail.com>
2016-11-07 21:46:28 +0100

    ts a js

---
+++ clase/listacomponent.js
@@ -0,0 +1,16 @@
+var MovimientosModule = (function () {
+    function MovimientosModule() {
+    }
+    MovimientosModule = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["H" /* NgModule */])({
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_1__angular_common__["b" /* CommonModule */],
+                __WEBPACK_IMPORTED_MODULE_5__shared_shared_module__["a" /* SharedModule */]
+            ],
+            declarations: [__WEBPACK_IMPORTED_MODULE_2__movimientos_component__["a" /* MovimientosComponent */], __WEBPACK_IMPORTED_MODULE_3__nuevo_nuevo_component__["a" /* NuevoComponent */], __WEBPACK_IMPORTED_MODULE_4__lista_lista_component__["a" /* ListaComponent */], __WEBPACK_IMPORTED_MODULE_6__fila_fila_component__["a" /* FilaComponent */]],
+            exports: [__WEBPACK_IMPORTED_MODULE_2__movimientos_component__["a" /* MovimientosComponent */]]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], MovimientosModule);
+    return MovimientosModule;
+}());
