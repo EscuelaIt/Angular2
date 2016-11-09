5ab674e74eaa8384eb11374a3bea040d058f8f67
Alberto Basalo <albertobasalo@agorabinaria.com>
2016-11-09 13:21:58 +0100

    2 clean

--- 2-modulos/cash-flow/src/app/index.ts
+++
@@ -1,6 +0,0 @@
-/** fichero índice que facilita la importación de otros ficheros
- *  si cualquier fichero indexado cambiase de ruta
- *  sólo tendríamos que tocar en este índice
- */
-export * from './app.component';
-export * from './app.module';
